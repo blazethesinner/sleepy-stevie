b9af341440cdc0bebe2f39ee2426fa2b4e4675aa
Totor <lucquinv@gmail.com>
2014-05-05 17:37:45 -0400

    Creating a unity project, and first dumby assets

---
+++ unity/Assets/scripts/playerMovementScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMovementScript : MonoBehaviour {
+
+	public float walkSpeed= 10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//dumb movement
+		if (Input.GetKey ("up"))
+		{
+			transform.Translate(Vector2.up * walkSpeed * Time.deltaTime);
+		}
+		if (Input.GetKey ("down"))
+		{
+			transform.Translate(-Vector2.up * walkSpeed *Time.deltaTime);
+		}
+		if (Input.GetKey ("left"))
+		{
+			transform.Translate(-Vector2.right * walkSpeed *Time.deltaTime);
+		}
+		if (Input.GetKey ("right"))
+		{
+			transform.Translate(Vector2.right * walkSpeed *Time.deltaTime);
+		}
+	}
+}
