ce1d4a69ae796fe163d9af216e00a6627fd1d41c
Totor <lucquinv@gmail.com>
2014-05-08 18:28:35 -0400

    some files forgotten in last commit. Done so far : a player, with physics gestion, a camera attached to him, and a sprite renderer. Regions. Obstacles (bush, rock). Rocks have collider and are solid.

---
+++ unity/Assets/scripts/levelGeneration/mapCreator.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mapCreator : MonoBehaviour {
+
+	public int width;
+	public int height;
+	public int regionHeight;
+	public int regionWidth;
+	public GameObject region;
+	public GameObject blockingRegion;
+
+	private int [,] matrix;
+
+
+	// Use this for initialization
+	void Start () {
+		matrix = new int[width, height];
+		fill (matrix);
+		createAreas (matrix);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void fill(int [,] matrix){
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++){
+				matrix [0, 0] = 1; // start
+				matrix[0,1]=1;
+				matrix[1,1]=1;
+				matrix[1,0]=1;
+			}
+		}
+	}
+
+	void createAreas (int[,] matrix){
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++){
+				int tmp = matrix[i,j];
+				switch (tmp){
+				case 1 :
+					Instantiate(region, new Vector3 (i*regionWidth,j*regionHeight,0), Quaternion.identity);
+					break;
+				case 2 :
+
+					break;
+				case 3 :
+
+					break;
+				}
+			}
+		}
+	}
+}
+
+
