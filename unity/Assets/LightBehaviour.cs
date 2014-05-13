b7eddbecbbab4e9adbac034d6a32eafb99c98feb
Totor <lucquinv@gmail.com>
2014-05-13 19:32:12 -0400

    Control of the character and player are now both ruled with awsd. This control is ruled only by the playermovementcontrol script. No more lightbehaviour script. Movement is now restricted to 4 directions.

--- unity/Assets/LightBehaviour.cs
+++ unity/Assets/LightBehaviour.cs
@@ -9,15 +9,19 @@ public class LightBehaviour : MonoBehaviour {
 
 	private bool isOn;
 	private float lastTimeOn;
+	private string direction;
+
 	// Use this for initialization
 	void Start () {
-		isOn = false;
-		GetComponent<SpriteRenderer>().sprite=nolight;
+		isOn = true;
+		GetComponent<SpriteRenderer>().sprite=flashlight;
 		lastTimeOn = Time.time;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		//turning on/off the light
 		if (Input.GetKey ("space") && ((Time.time-lastTimeOn)>0.1))
 		{
 			lastTimeOn=Time.time;
@@ -29,5 +33,45 @@ public class LightBehaviour : MonoBehaviour {
 				GetComponent<SpriteRenderer>().sprite=nolight;
 			}
 		}
+
+		//changing direction accordingly to inputs
+		if (Input.GetKey ("w")) {
+			direction = "up";	
+		}
+		if (Input.GetKey ("s")) {
+			direction = "down";
+		}
+		if (Input.GetKey ("a")) {
+			direction = "left";
+		}
+		if (Input.GetKey ("d")) {
+			direction = "right";
+		}
+
+		//changing transform accordingly to direction
+		if (isOn) {
+			if (direction=="up") {
+				transform.localEulerAngles = new Vector3 (0, 0, 270);
+				transform.localPosition = new Vector3 (0, 8, 0);
+			}
+			if (direction=="down") {
+				transform.localEulerAngles = new Vector3 (0, 0, 90);
+				transform.localPosition = new Vector3 (0, -8, 0);
+			}
+			if (direction=="left") {
+				transform.localEulerAngles = new Vector3 (0, 0, 0);
+				transform.localPosition = new Vector3 (-8, 0, 0);
+			}
+			if (direction=="right") {
+				transform.localEulerAngles = new Vector3 (0, 0, 180);
+				transform.localPosition = new Vector3 (8, 0, 0);
+			}
+		}
+		else {
+			transform.localEulerAngles = new Vector3 (0, 0, 0);
+			transform.localPosition = new Vector3 (3,-1, 0);
+		}
+
+
 	}
 }
--- unity/Assets/scripts/playerMovementScript.cs
+++ unity/Assets/scripts/playerMovementScript.cs
@@ -5,34 +5,95 @@ public class playerMovementScript : MonoBehaviour {
 
 	public float walkSpeed= 10;
 
-	// Use this for initialization
-	void Start () {
+	public Sprite left;
+	public Sprite right;
+	public Sprite up;
+	public Sprite down;
+
+	public Sprite flashlight;
+	public Sprite nolight;
 	
+	private bool isOn;
+	private float lastTimeOn;
+	private string direction;
+
+	private GameObject spriterenderer;
+	private GameObject light;
+	// Use this for initialization
+
+	void Start () {
+		spriterenderer = GameObject.Find ("playerSprite");
+		light = GameObject.Find ("Light");
+		isOn = true;
+		light.GetComponent<SpriteRenderer>().sprite=flashlight;
+		lastTimeOn = Time.time;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//changing direction according to inputs
+		if (Input.GetKey ("w") && !Input.GetKey ("s") && !Input.GetKey ("a") && !Input.GetKey ("d")) {
+			direction = "up";	
+		}
+		if (!Input.GetKey ("w") && Input.GetKey ("s") && !Input.GetKey ("a") && !Input.GetKey ("d")) {
+			direction = "down";
+		}
+		if (!Input.GetKey ("w") && !Input.GetKey ("s") && Input.GetKey ("a") && !Input.GetKey ("d")) {
+			direction = "left";
+		}
+		if (!Input.GetKey ("w") && !Input.GetKey ("s") && !Input.GetKey ("a") && Input.GetKey ("d")) {
+			direction = "right";
+		}
+
+		//light direction
+		if (isOn) {
+			if (direction=="up") {
+				light.transform.localEulerAngles = new Vector3 (0, 0, 270);
+				light.transform.localPosition = new Vector3 (0, 8, 0);
+			}
+			if (direction=="down") {
+				light.transform.localEulerAngles = new Vector3 (0, 0, 90);
+				light.transform.localPosition = new Vector3 (0, -8, 0);
+			}
+			if (direction=="left") {
+				light.transform.localEulerAngles = new Vector3 (0, 0, 0);
+				light.transform.localPosition = new Vector3 (-8, 0, 0);
+			}
+			if (direction=="right") {
+				light.transform.localEulerAngles = new Vector3 (0, 0, 180);
+				light.transform.localPosition = new Vector3 (8, 0, 0);
+			}
+		}
+		else {
+			light.transform.localEulerAngles = new Vector3 (0, 0, 0);
+			light.transform.localPosition = new Vector3 (3,-1, 0);
+		}
+
 
 		//dumb movement
-		if (Input.GetKey ("w"))
-		{
-			//rigidbody2D.AddForce(Vector2.up*10);
-			transform.Translate(Vector2.up * walkSpeed * Time.deltaTime);
+		if (direction == "up") {
+			if (Input.GetKey ("w")) 
+				transform.Translate (Vector2.up * walkSpeed * Time.deltaTime);
+			spriterenderer.GetComponent<SpriteRenderer> ().sprite = up;
 		}
-		if (Input.GetKey ("s"))
-		{
-			//rigidbody2D.AddForce(-Vector2.up*10);
-			transform.Translate(-Vector2.up * walkSpeed *Time.deltaTime);
+
+		if (direction == "down") {
+			if (Input.GetKey ("s")) 
+				transform.Translate (-Vector2.up * walkSpeed * Time.deltaTime);
+			spriterenderer.GetComponent<SpriteRenderer> ().sprite = down;
 		}
-		if (Input.GetKey ("a"))
-		{
-			//rigidbody2D.AddForce(-Vector2.right*10);
-			transform.Translate(-Vector2.right * walkSpeed *Time.deltaTime);
+
+		if (direction == "left") {
+			if (Input.GetKey ("a")) 
+				transform.Translate (-Vector2.right * walkSpeed * Time.deltaTime);
+			spriterenderer.GetComponent<SpriteRenderer> ().sprite = left;
 		}
-		if (Input.GetKey ("d"))
-		{
-			//rigidbody2D.AddForce(Vector2.right*10);
-			transform.Translate(Vector2.right * walkSpeed *Time.deltaTime);
+		if (direction == "right") {
+			if (Input.GetKey ("d")) 
+				transform.Translate (Vector2.right * walkSpeed * Time.deltaTime);
+			spriterenderer.GetComponent<SpriteRenderer> ().sprite = right;
 		}
 	}
+
+
 }
