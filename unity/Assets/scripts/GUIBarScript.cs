86f99cc06348f10b2bc233775f16ae6e75e5a367
Totor <lucquinv@gmail.com>
2014-05-20 19:01:35 -0400

    first draft on GUI bar, added font. Deleted the gameobject tent, attached the tent script to the prefab tent.

---
+++ unity/Assets/scripts/GUIBarScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIBarScript : MonoBehaviour {
+
+	public GUIStyle styleButtons;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void OnGUI () {
+		GUI.Label (new Rect (100, 30, 100, 50), "Battery :", styleButtons);
+		GUI.Label (new Rect (200, 30, 100, 50), "Life :", styleButtons);
+		GUI.Label (new Rect (300, 30, 100, 50), "Time :", styleButtons);
+	}
+}
