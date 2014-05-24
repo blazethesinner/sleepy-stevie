7c53e4687e404b1cdc86067343faf4cdf761a049
Totor <lucquinv@gmail.com>
2014-05-23 21:46:14 -0400

    better menu, thanks to Alyssa's wonderfull arts, but with fucking glow effects. She disobeyed me.

--- unity/Assets/scripts/otherScenes/menu.cs
+++ unity/Assets/scripts/otherScenes/menu.cs
@@ -20,17 +20,18 @@ public class menu : MonoBehaviour {
 	void OnGUI () {
 
 		if (currentState == state.main) {
-			if (GUI.Button (new Rect (Screen.width / 2 - 150, Screen.height / 2, 300, 50), "New Game", styleButtons)) {
+			if (GUI.Button (new Rect (Screen.width / 2 - 150, Screen.height / 2, 300, 75), "New Game", styleButtons)) {
 				Application.LoadLevel ("game");
 			}
-			if (GUI.Button (new Rect (Screen.width / 2 - 150, Screen.height / 2 + 100, 300, 50), "Options", styleButtons)) {
+			if (GUI.Button (new Rect (Screen.width / 2 - 150, Screen.height / 2 + 100, 300, 75), "Options", styleButtons)) {
 				currentState = state.options;
 			}
-			if (GUI.Button (new Rect (Screen.width / 2 - 150, Screen.height / 2 + 200, 300, 50), "Leave Game", styleButtons)) {
+			if (GUI.Button (new Rect (Screen.width / 2 - 150, Screen.height / 2 + 200, 300, 75), "Leave Game", styleButtons)) {
 				Application.Quit ();
 			}
-
-			GUI.Label (new Rect (100, 100, 760, 100), "Sleepy Stevie", titleStyle);
+			GameObject.Find("SleepyStevie").GetComponent<SpriteRenderer>().enabled = true;
+			GameObject.Find("Options").GetComponent<SpriteRenderer>().enabled = false;
+			//GUI.Label (new Rect (100, 100, 760, 100), "Sleepy Stevie", titleStyle);
 		}
 
 		if (currentState == state.options) {
@@ -48,6 +49,8 @@ public class menu : MonoBehaviour {
 			
 			if (GUI.Button (new Rect (Screen.width / 2 - 100, Screen.height / 2 + 200, 200, 50), "Back To Menu", styleButtons))
 				currentState=state.main;
+			GameObject.Find("SleepyStevie").GetComponent<SpriteRenderer>().enabled = false;
+			GameObject.Find("Options").GetComponent<SpriteRenderer>().enabled = true;
 		}
 	}
 
