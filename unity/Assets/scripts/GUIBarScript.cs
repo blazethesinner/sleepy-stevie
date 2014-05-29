cd2335980d42095a8c026ba03a668ecef56161a4
MRBucci <mrbucci617@gmail.com>
2014-05-29 17:37:23 -0400

    GUI elements now have placeholder graphics and sound has been added, but not hooked up yet,

--- unity/Assets/scripts/GUIBarScript.cs
+++ unity/Assets/scripts/GUIBarScript.cs
@@ -7,6 +7,8 @@ public class GUIBarScript : MonoBehaviour {
 	public GUIStyle flashlightBar;
 	public Texture emptyBattery;
 	public Texture fullBattery;
+	public Texture emptyHealth;
+	public Texture fullHealth;
 	// Use this for initialization
 	void Start () {
 		
@@ -17,7 +19,35 @@ public class GUIBarScript : MonoBehaviour {
 		GUI.Label (new Rect (100, 30, 100, 50), "Battery :" + ((int)LightBehaviour.batteryLife).ToString() + "%", styleButtons);
 		GUI.Label (new Rect (200, 30, 100, 50), "Life :" + playerMovementScript.life, styleButtons);
 		GUI.Label (new Rect (300, 30, 100, 50), "Time :" + (int)Time.time, styleButtons);
-
+		fullBattery = (Texture) Resources.Load ("fullBattery");
+		emptyBattery = (Texture) Resources.Load ("emptyBattery"); 
+		fullHealth = (Texture) Resources.Load ("fullHealth");
+		emptyHealth = (Texture) Resources.Load ("emptyHealth"); 
+		
+		if(!fullBattery || !emptyBattery || !fullHealth || !emptyHealth){
+			Debug.LogError("Assign a GUI Texture in the inspector.");
+			return;
+		}
+		
+		
+		GUI.DrawTexture (new Rect (100, 30, 100, 50), emptyBattery);
+		
+		GUI.BeginGroup (new Rect (100, 30, (int)LightBehaviour.batteryLife, 50));
+		GUI.DrawTexture (new Rect (0, 0, 100, 50), fullBattery);
+		
+		GUI.EndGroup ();
+		
+		
+		
+		
+		
+		GUI.DrawTexture (new Rect (200, 30, 100, 50), emptyHealth);
+		//GUI.DrawTexture (new Rect (200, 30, 100/3 * (int)playerMovementScript.life, 50), fullHealth);
+		
+		GUI.BeginGroup (new Rect (200, 30, 100/3 * (int)playerMovementScript.life, 50));
+		GUI.DrawTexture (new Rect (0, 0, 100, 50), fullHealth);
+		
+		GUI.EndGroup ();
 
 		/* Still working on image
 		 * GUI.DrawTexture (new Rect (100, 30, 100, 50), fullBattery);
