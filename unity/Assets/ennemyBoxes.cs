83214dd7f647c32a62dd543906fc4b0534ce837e
Totor <lucquinv@gmail.com>
2014-05-29 16:00:13 -0400

    work on rabbit's behaviour (AI and collision)

---
+++ unity/Assets/ennemyBoxes.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ennemyBoxes : MonoBehaviour {
+	public GameObject rabbit;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	//void OnTriggerEnter2D(Collider2D other)
+	//{
+	//	if (other.gameObject.tag=="obstacle")
+	//	rabbit.SendMessage (this.name+"enter");
+	//}
+	void OnTriggerStay2D(Collider2D other)
+	{
+		if (other.gameObject.tag=="obstacle")
+		rabbit.SendMessage (this.name+"stay");
+	}
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.gameObject.tag=="obstacle")
+		rabbit.SendMessage (this.name+"exit");
+	}
+}
--- unity/Assets/scripts/ennemies/RabbitBehaviour.cs
+++ unity/Assets/scripts/ennemies/RabbitBehaviour.cs
@@ -3,27 +3,41 @@ using System.Collections;
 
 public class RabbitBehaviour :  MonoBehaviour {
 
+	//characteristics
 	public int maxHealth;
 	public int walkSpeed;
-
 	private int health;
+
+	//enums
 	public enum state {light, dark};
 	public enum behaviour {noMove, chasingTarget, wander};
 	public enum order {stay, right, left, up, down};
-	
-	public state myState;
+
+	//public state myState;
 	public behaviour myBehaviour;
 	public order myOrder;
 
+	//references to game objects
+
 	public GameObject map;
-
-
 	//collider boxes for detection of walls
 	private BoxCollider2D leftBox;
 	private BoxCollider2D rightBox;
 	private BoxCollider2D topBox;
 	private BoxCollider2D botBox;
 
+
+
+	//timers
+	private float timeStaying;
+
+	//others
+
+	public order wallFollowed;
+	public bool obstacleLeft;
+	public bool obstacleRight;
+	public bool obstacleTop;
+	public bool obstacleBot;
 
 	
 	// Use this for initialization
@@ -37,6 +51,7 @@ public class RabbitBehaviour :  MonoBehaviour {
 			return "";
 		}
 	}
+
 	public static string toString (behaviour beh){
 		switch (beh) {
 		case behaviour.chasingTarget :
@@ -68,43 +83,206 @@ public class RabbitBehaviour :  MonoBehaviour {
 
 
 	void Start(){
+		myBehaviour = behaviour.wander;
 		myOrder = order.stay;
+		timeStaying = 0;
 		map = GameObject.Find ("map");
 	}
 
 	void Update(){
-		updateState ();
+		//updateState ();
 		computeAI ();
 		applyOrder ();
+
+		//timers
+		timeStaying += Time.deltaTime;
+
+
+		//reset obstacles
+		/*obstacleBot = false;
+		obstacleLeft = false;
+		obstacleRight = false;
+		obstacleTop = false;
+		*/
 	}
 
 	//debug
 	
 	public void updateState()// change state depending on inlight or not, and changes characteristics
 	{
-		myState = state.light; //for now, the state doesn't change
-
-
-
 	} 
 
 	public void computeAI()//change order and behaviour depending on lots of things
 	{
-		/*
-		if (myOrder == order.left) {
-			int tmp = whatsIn(transform.position);
-			if (tmp==1 || tmp ==2 || tmp == 3 || tmp == 4)
-				myOrder= order.right;
-		}
-		else{
-			if (myOrder == order.right){
-				int tmp = whatsIn(transform.position-Vector3.left);
-				if (tmp==1 || tmp ==2 || tmp == 3 || tmp == 4)
-					myOrder= order.left;
+		if (myBehaviour==behaviour.wander){
+			/*
+			//todo here : detect player
+
+			// if wandering, the ai changes direction if one of the two following conditions are true :
+			// - walking in a direction where there is an obstacle
+			// - following a wall that "ends"
+			switch (wallFollowed) {
+			case order.down :
+				if (!obstacleBot){
+					changeDirectionWander();
+				}
+				break;
+			case order.up :
+				if (!obstacleTop){
+					changeDirectionWander();
+				}
+				break;
+			case order.left :
+				if (!obstacleLeft){
+					changeDirectionWander();
+				}
+				break;
+			case order.right :
+				if (!obstacleRight){
+					changeDirectionWander();
+				}
+				break;
+
+			case order.stay :
+				wallFollowed = findWallFollowed();
+				break;
 			}
+
+			switch (myOrder){
+			case order.down :
+				if (obstacleBot){
+					changeDirectionWander();
+					wallFollowed=order.down;
+				}
+				break;
+			case order.up :
+				if (obstacleTop){
+					changeDirectionWander();
+					wallFollowed=order.up;
+				}
+				break;
+			case order.left :
+				if (obstacleLeft){
+					changeDirectionWander();
+					wallFollowed=order.left;
+				}
+				break;
+			case order.right :
+				if (obstacleRight){
+					changeDirectionWander();
+					wallFollowed=order.right;
+				}
+				break;
+				
+			default :
+				break;
+			}
+			*/
 		}
-		*/
+	
 	} 
+
+	public void changeDirectionWander(){
+		print ("change");
+		bool trapped = obstacleBot && obstacleTop && obstacleLeft && obstacleRight;
+		if (trapped)
+			myOrder = order.stay;
+		else
+		{
+			order testdirection=order.stay;
+			while (isObstacle(testdirection)){
+				int randomNumber = Random.Range (0, 4);
+				switch(randomNumber){
+				case 0 :
+					testdirection=order.down;
+					break;
+				case 1 :
+					testdirection=order.up;
+					break;
+				case 2 :
+					testdirection=order.left;
+					break;
+				case 3 :
+					testdirection=order.right;
+					break;
+				}
+			}
+
+			myOrder=testdirection;
+			wallFollowed=findWallFollowed();
+			print ("new dir : " + toString(testdirection)+ " , new wall : " + toString(wallFollowed));
+
+		}
+	}
+
+	order findWallFollowed(){
+		if (!obstacleBot && !obstacleTop && !obstacleLeft && !obstacleRight) //don't follow a wall : keep not following a wall
+			return order.stay;
+
+		switch (myOrder) {
+		case order.down:
+			if (obstacleRight){
+				if (obstacleLeft) {
+					if (Random.Range (0, 1) == 0)
+						return order.right;
+					else
+						return order.left;
+					}
+					return order.right;
+				}
+			else
+				return order.left;
+			break;
+		case order.up :
+			if (obstacleRight){
+				if (obstacleLeft) {
+					if (Random.Range (0, 1) == 0)
+						return order.right;
+					else
+						return order.left;
+				}
+				return order.right;
+			}
+			else
+				return order.left;
+			break;
+
+		case order.right :
+			if (obstacleTop){
+				if (obstacleBot) {
+					if (Random.Range (0, 1) == 0)
+						return order.up;
+					else
+						return order.down;
+				}
+				return order.up;
+			}
+			else
+				return order.down;
+			break;
+
+		case order.left :
+			if (obstacleTop){
+				if (obstacleBot) {
+					if (Random.Range (0, 1) == 0)
+						return order.up;
+					else
+						return order.down;
+				}
+				return order.up;
+			}
+			else
+				return order.down;
+			break;
+			break;
+
+		default :
+			return order.stay;
+			break;
+		}
+
+	}
+
 	
 	public void applyOrder()//move sprite depending on order, play animation according to state, etc
 	{
@@ -126,15 +304,70 @@ public class RabbitBehaviour :  MonoBehaviour {
 		}
 	}
 
-	int whatsIn (Vector3 vec){
-		int xR = (int)((vec.x) / 19);
-		int yR = (int)((vec.y) / 13);
-		int xB = (int)((vec.x) % 19);
-		int yB = (int)((vec.y) % 13);
-		GameObject myRegion = map.GetComponent<mapCreator> ().regionMatrix [yR, xR];
-		int ans = myRegion.GetComponent<regionCreator> ().matrix [yB, xB];
-		print (ans);
-		return ans;
+
+	void leftstay()
+	{
+		obstacleLeft = true;
+		if (myOrder == order.left)
+			changeDirectionWander ();
+	}
+	void rightstay()
+	{
+		obstacleRight = true;
+		if (myOrder == order.right)
+			changeDirectionWander ();
+	}
+	void topstay()
+	{
+		obstacleTop = true;
+		if (myOrder == order.up)
+			changeDirectionWander ();
+	}
+	void bottomstay()
+	{
+		obstacleBot = true;
+		if (myOrder == order.down)
+			changeDirectionWander ();
+	}
+	void leftexit()
+	{
+
+		if (!obstacleLeft && wallFollowed == order.left)
+			changeDirectionWander ();
+		obstacleLeft = false;
+	}
+	void rightexit()
+	{
+		obstacleRight = false;
+		if (!obstacleRight && wallFollowed == order.right)
+			changeDirectionWander ();
+	}
+	void topexit()
+	{
+		obstacleTop = false;
+		if (!obstacleTop && wallFollowed == order.up)
+			changeDirectionWander ();
+	}
+	void bottomexit()
+	{
+		obstacleBot = false;
+		if (!obstacleBot && wallFollowed == order.down)
+			changeDirectionWander ();
+	}
+
+	bool isObstacle (order thisorder){
+		switch(thisorder){
+		case order.down :
+			return obstacleBot;
+		case order.left :
+			return obstacleLeft;
+		case order.right :
+			return obstacleRight;
+		case order.up :
+			return obstacleTop;
+		default :
+			return true;
+		}
 	}
 
 }
